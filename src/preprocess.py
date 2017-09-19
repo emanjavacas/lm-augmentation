b58f7e0effcafed49ef148ca45802539224d118f
Enrique Manjavacas <enrique.manjavacas@gmail.com>
2017-09-19 15:30:21 +0200

    moved preprocessing code

---
+++ src/preprocess.py
@@ -0,0 +1,163 @@
+
+import os
+import csv
+import warnings
+from collections import Counter
+
+try:
+    import ucto
+except ImportError:
+    warnings.warn("Couldn't import ucto; using dummy tokenizer")
+    ucto = None
+
+
+def make_tokenizer(lang):
+    if ucto is not None:
+        return ucto.Tokenizer("tokconfig-{lang}".format(lang=lang),
+                              paragraphdetection=True)
+
+
+def split_sentences(par, tokenizer):
+    prev_ending = [], False
+    if tokenizer is not None:
+        tokenizer.process(par)
+        sentence = ''
+        for token in tokenizer:
+            if prev_ending and token.isbeginofsentence():
+                sentence = sentence.strip()
+                if sentence:
+                    yield sentence
+                    sentence = ''
+            prev_ending = token.isendofsentence()
+            sentence += str(token)
+            if not token.nospace():
+                sentence += ' '
+        if sentence:
+            sentence = sentence.strip()
+            yield sentence
+    else:
+        for line in par.split('\n'):
+            yield line.split()
+
+
+def split_pars(filename):
+    buf, prev_par = '', False
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                if prev_par:
+                    continue
+                yield buf
+                buf, prev_par = '', True
+            else:
+                buf += line + '\n'
+                prev_par = False
+
+
+def compute_length(l, length_bins=(50, 100, 150, 300)):
+    length = len(l)
+    output = None
+    for length_bin in length_bins[::-1]:
+        if length > length_bin:
+            output = length_bin
+            break
+    else:
+        output = -1
+    return output
+
+
+class Reader(object):
+    def __init__(self, path, tokenizer):
+        self.fileiter = [os.path.join(path, f) for f in os.listdir(path)]
+        self.tokenizer = tokenizer
+
+    def on_filename(self, filename):
+        "Callback to extract sentence labels from filename"
+        return [filename.split('_')[0]]
+
+    def on_par(self, par):
+        "Callback to extract sentence labels at the paragraph level"
+        return []
+
+    def on_sent(self, sent):
+        "Callback to extract sentence labels from the sentence itself"
+        return [compute_length(sent)]
+
+    def sent_length(self, sent):
+        "Compute sentence length. Default implementation uses words"
+        return len(sent)
+
+    def process(self, shuffle_files=True, shuffle_pars=False,
+                balance=False, balance_ref=None, balance_count=None):
+        """
+        Generator over labeled sentences.
+
+        Parameters:
+        -----------
+        balance: bool, whether to balance output on count based on a given
+            sentence label such as author.
+        balance_ref: None or int, integer pointing to the position in the
+            sentence label array of the label to be used for balancing
+        balance_count: None or int, maximum count per balancing label
+        """
+        if shuffle_files:
+            random.shuffle(self.fileiter)
+        pars = []
+        for f in self.fileiter:
+            file_labels = self.on_filename(os.path.basename(f))
+            for par in split_pars(f):
+                par_labels = self.on_par(par)
+                pars.append((file_labels + par_labels, par))
+        if shuffle_pars:
+            random.shuffle(pars)
+        if balance is not None:
+            balance_counter = Counter(int)
+        for labels, par in pars:
+            for sent in split_sentences(par, self.tokenizer):
+                sent_labels = labels + self.on_sent(sent)
+                if balance is not None:
+                    sent_length = self.sent_length(sent)
+                    ref = sent_labels[balance_ref]
+                    balance_counter[ref] += sent_length
+                    if balance_counter[ref] > balance_count:
+                        continue
+                yield sent_labels, sent
+
+
+def writelines(outputfile, rows, include_lengths=True):
+    with open(outputfile + '.csv', 'w+', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile, delimiter='\t')
+        for ls, sent in rows:
+            csvwriter.writerow([*ls, sent])
+
+
+def readlines(inputfile):
+    with open(inputfile, 'r', newline='\n') as f:
+        for line in f:
+            *labels, sent = line.split('\t')
+            yield labels, sent
+
+
+if __name__ == '__main__':
+    import argparse
+    import random
+    parser = argparse.ArgumentParser(
+        description="Compute train, test splits for an input corpus")
+    parser.add_argument('corpus')
+    parser.add_argument('outputdir')
+    parser.add_argument('--train_size', default=0.75, type=float)
+    parser.add_argument('--seed', default=1000, type=int)
+    parser.add_argument('--lang', default='eng')
+    args = parser.parse_args()
+
+    random.seed(args.seed)
+    reader = Reader(args.corpus, make_tokenizer(args.lang))
+    rows = list(reader.process(shuffle_files=True, shuffle_pars=True))
+    split = int(len(rows) * args.train_size)
+    train_rows, test_rows = rows[:split], rows[-split:]
+
+    if not os.path.exists(args.outputdir):
+        os.makedirs(args.outputdir)
+    writelines(os.path.join(args.outputdir, 'train'), train_rows)
+    writelines(os.path.join(args.outputdir, 'test'), test_rows)
